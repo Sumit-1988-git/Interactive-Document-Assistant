041bd0cbd311df4b84f43f95d189d08a48f5c05b
Sumit Kumar Sahu <sumitkumarsahu1988@gmail.com>
2025-09-06 12:21:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,15 +52,14 @@ retrieval_chain = create_retrieval_chain(retriever, document_chain)
 st.title("Interactive Document Assistant")
 
 # Instructions on how to use the assistant
-st.markdown(
-    f'<div class="instructions" style="color: lightblue;">
+st.markdown( <div class="instructions" style="color: lightblue;">
         **Instructions:**
         
         1. Type your question in the text input below and click "Get Answer".
         
         2. The assistant will respond based on the content of the PDFs.
         
-    </div>'
+    </div>
   , unsafe_allow_html=True)
 
 # Input field for user's question
@@ -77,6 +76,7 @@ if st.button("Get Answer"):
         st.markdown(f'<div class="chat-box"><div class="chat-bubble bot" style="color: lightgreen; padding: 10px; border-radius: 15px;">{response["answer"]}</div></div>', unsafe_allow_html=True)
     else:
         st.write("Please enter a question.")
+
 
 
 
